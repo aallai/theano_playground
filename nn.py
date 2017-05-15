9bd6f177a782cfb43db35a822b259ca690fb456a
aallai <alexandre.allaire@mail.mcgill.ca>
2017-05-14 17:49:55 -0700

    Add ReluFC.

--- nn.py
+++ nn.py
@@ -1,25 +1,7 @@
 import theano
 import numpy
 
-INPUT_CHANNELS = 1
-OUTPUT_CHANNELS = 32
-SCREEN_BUFFER_SIZE = 84
-FILTER_SIZE = 8
-FILTER_STRIDE = 4
-BATCH_SIZE = 1
-
-#filters = [[numpy.random.randn(FILTER_SIZE, FILTER_SIZE)] for i in range(OUTPUT_CHANNELS)]
-
-#input = numpy.random.randn(SCREEN_BUFFER_SIZE * SCREEN_BUFFER_SIZE)
-
-input = theano.tensor.tensor4('input')
-filters = theano.tensor.tensor4('filters')
-
-convnet = theano.tensor.nnet.conv2d(
-    input, filters,
-    input_shape = (BATCH_SIZE, INPUT_CHANNELS, SCREEN_BUFFER_SIZE, SCREEN_BUFFER_SIZE),
-    filter_shape = (OUTPUT_CHANNELS, INPUT_CHANNELS, FILTER_SIZE, FILTER_SIZE),
-    border_mode = 'valid', subsample=(FILTER_STRIDE, FILTER_STRIDE))
+from theano import tensor as T
 
 class ReluConv:
 
@@ -27,8 +9,25 @@ class ReluConv:
     def __init__(self, input, input_size, filter_size, stride, input_channels, output_channels, relu_leak):
 
 
+        # TODO: look up how to initialze weights.
         self.filters = theano.shared(value = [[numpy.random.randn(filter_size, filter_size) for i in range(input_channels)] for i in range(output_channels)], borrow = True)
 
-        self.convnet = theano.tensor.nnet.conv2d(input, self.filters, border_mode = 'valid', subsample = (stride, stride))
+        self.convnet = T.nnet.conv2d(input, self.filters, border_mode = 'valid', subsample = (stride, stride))
 
-        self.output = theano.tensor.relu(self.convnet, relu_leak)
+        self.output = T.nnet.relu(self.convnet, relu_leak)
+
+class ReluFC:
+
+    def __init__(self, input, input_dim, output_dim, linear = False, relu_leak = 0):
+
+        # W and b are actually tranposed here to accomodate the row-vector format of the input.
+        self.W = theano.shared(value = numpy.random.randn(input_dim, ouput_dim), name = 'W')
+        self.b = theano.shared(value = numpy.random.randn(output_dim), name = 'b')
+
+        activation = T.dot(input, self.W) + self.b
+
+        if linear:
+            self.output = activation
+        else:
+            self.output = T.nnet.relu(activation, relu_leak)
+
